3032141b7b13871abf6e5a790a7ccb928b71c4a1
Patrick Bukenberger <patrick.bukenberger@gmail.com>
2020-02-10 10:10:07 +0100

    first

---
+++ selectors/myscript.js
@@ -0,0 +1,38 @@
+/*
+ * My jQuery selectors
+ *
+ * Apply a style such as a yellow background to the following elements.
+ * Test your selectors one at a time.
+ */
+
+// all elements
+// $("*").css("background-color", "yellow");
+
+// all p elements
+// $("p").css("background-color", "yellow");
+
+// the p element with an ID of "intro"
+// $("#intro").css("background-color", "yellow");
+
+// all elements with a class of "note"
+// $(".note").css("background-color", "yellow");
+
+// all DIV elements with a class of note
+// $("div.note").css("background-color", "yellow");
+
+// all p elements in the article element
+
+
+// the last element in any section which is a p tag
+
+
+// the first paragraph on the page
+
+
+// all p elements which have a link
+
+
+// the second li within nested ul element
+
+
+// every second and third p element
